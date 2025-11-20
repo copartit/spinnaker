6ec21f46a243d00d29cdaf3840014e24f310173f
Igor Cherkaev <igor.cherkaev@copart.com>
2025-11-20 12:39:15 -0600

    Custom build files for deck, clouddriver, front50. Custom .gitignore and .dockerignore
    (cherry picked from commit a2ccf5424703c8587ab0f664c5b96a900487e827)

---
+++ front50/docker_build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+APP="front50"
+BASE_VER="$(git branch --show-current --no-color)"
+BUILD=`cat ../.idea/build_n.txt`
+
+if [ -z ${APP} ]
+then
+  echo "Pick app to build, e.g. front50"
+  exit 1
+fi
+
+if [ -z ${BUILD} ]
+then
+   BUILD=3
+else
+   BUILD=$((BUILD+1))
+fi
+
+BUILD_OPTS="${BUILD_OPTS} --progress=plain"
+
+echo "Building docker image, build number ${BUILD}"
+TAG="dockerregistry.copart.com/devops/spinnaker-${APP}:${BASE_VER}-${BUILD}"
+docker build $BUILD_OPTS -t ${TAG} --build-arg APP=${APP} -f ./Dockerfile ../
+
+echo -n ${BUILD} > ../.idea/build_n.txt
+echo "Build complete: ${TAG}"
