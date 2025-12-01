0a10b794b8c1a42cf9d8873decf6fdaccd6e5d98
Igor Cherkaev <igor.cherkaev@copart.com>
2025-12-01 15:16:49 -0600

    rosco: add dockerfile and build files
    Signed-off-by: Igor Cherkaev <igor.cherkaev@copart.com>

---
+++ rosco/docker_build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+APP="rosco"
+BASE_VER="$(git branch --show-current --no-color)"
+BUILD=`cat ../.idea/build_n.txt`
+
+if [ -z ${BUILD} ]
+then
+   BUILD=3
+else
+   BUILD=$((BUILD+1))
+fi
+
+BUILD_OPTS="${BUILD_OPTS} --progress=plain"
+
+echo "Building docker image, build number ${BUILD}"
+TAG="dockerregistry.copart.com/devops/spinnaker-${APP}:${BASE_VER}-${BUILD}"
+docker build $BUILD_OPTS -t ${TAG} --build-arg APP=${APP} -f ./Dockerfile ../
+
+echo -n ${BUILD} > ../.idea/build_n.txt
+echo "Build complete: ${TAG}"
